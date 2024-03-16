d947fae05bd214fa519afcb7c4bf24f4891b85f3
DarkB0ts <prasanna110600@gmail.com>
2024-03-16 16:43:13 +0530

    add route

--- fastapi_api/main.py
+++ fastapi_api/main.py
@@ -1,17 +1,11 @@
 from fastapi import FastAPI, File, UploadFile, Form
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
-# from nlp_model.ml_model import *
-# from celery_work.worker import score_resume_task
-# import re
-# import os
-# import redis
-
-# import sys
-# import os
-
-# # Add the current directory to the Python path
-# sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+from nlp_model.ml_model import *
+from celery_work.worker import score_resume_task
+import re
+import os
+import redis
 
 
 app = FastAPI()
@@ -28,33 +22,27 @@ async def read_root():
 
 @app.post('/process')
 async def process_resumes(job_tags: list, job_description: str, files: list[UploadFile] = File(...)):
-    res = []
-    for i in files:
-        data = {
-            'job_tags': job_tags,
-            'job_description': job_description,
-            'file': i.filename
-        }
-        res.append(data)
-    # filtered_files = []
+    filtered_files = []
 
-    # task_done=[]
+    task_done = []
 
-    # for resume in files:
+    for resume in files:
 
-    #     filename = resume.filename
+        filename = resume.filename
 
-    #     file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
 
-    #     resume.file.seek(0)
+        resume.file.seek(0)
 
-    #     with open(file_path, "wb") as buffer:
+        with open(file_path, "wb") as buffer:
 
-    #         buffer.write(resume.file.read())
-    #     preprocessed_text = predictResume(os.path.join(app.config['UPLOAD_FOLDER'], resume))
+            buffer.write(resume.file.read())
+        preprocessed_text = predictResume(
+            os.path.join(app.config['UPLOAD_FOLDER'], resume))
 
-    #     task_process,task = score_resume_task.delay(preprocessed_text,job_tags, job_description, files, file_path)
+        task_process, task = score_resume_task.delay(
+            preprocessed_text, job_tags, job_description, files, file_path)
 
-    #     if task_process:
-    #         task_done.append(task[1])
+        if task_process:
+            task_done.append(task[1])
     return JSONResponse(content={"res": res})
